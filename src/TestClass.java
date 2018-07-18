c66b2887252abf3a3dd6011d7de41373710dfc5b
mahdi <mahdihp.devsc@hotmail.com>
2018-07-18 10:53:46 +0430

    Tir 27.4

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,7 +1,12 @@
 public class TestClass {
     public static void main(String[] args) {
 
-
+        long l1 = 2; // upCasting primitive
+        long l2 = 2l; //
+        Long l3 = new Long(2l);
+        Long l4 = new Long(2);// upCasting primitive
+        Long l5 = 2L;// AutoBoxing
+        Long l6 = 2; //Compile Error
     }
 
 }
