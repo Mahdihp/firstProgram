92a12a4aa66fe3c6bc8149ac3a4a23b0d7eb1886
mahdi <mahdihp.devsc@hotmail.com>
2018-07-25 16:49:45 +0430

    3 Mordad قبل از اسلاید 10 version : 1

---
+++ src/mordad_3th/entity/Entity.java
@@ -0,0 +1,25 @@
+package mordad_3th.entity;
+
+public class Entity {
+
+    private int id;
+
+    public Entity(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Entity{" +
+                "id=" + id +
+                '}';
+    }
+}
