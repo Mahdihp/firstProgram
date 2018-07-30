a80807b3a601fdcd97568f1739acc464212f1b62
mahdi <mahdihp.devsc@hotmail.com>
2018-07-30 20:18:04 +0430

    تمرین 6 قسمت جاوایی

--- src/tamrin_6/Java_part/TestClass.java
+++ src/tamrin_6/Java_part/TestClass.java
@@ -2,7 +2,7 @@ package tamrin_6.Java_part;
 
 public class TestClass {
     public static void main(String[] args) {
-        Library library=new Library();
+        Library library=new Library("");
 
     }
 }
